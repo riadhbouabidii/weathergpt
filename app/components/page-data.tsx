0e70ba10fcd04fe299ff9fcfde027bb1940ab4d8
riadhbouabidii <136456187+riadhbouabidii@users.noreply.github.com>
2023-06-19 22:57:48 +0100

    Update page-data.tsx

--- app/components/page-data.tsx
+++ app/components/page-data.tsx
@@ -7,7 +7,7 @@ export function PageData({ data }: { data: any }) {
     <>
       <main>
         
-        <h1>Get the weather of any given location!</h1>
+        <h1>Get the weather of your location!</h1>
         <Illustration />
         <div className="meta">
           <div className="info">
